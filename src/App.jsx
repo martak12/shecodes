240379b4cb2e6e8e8affe3b6ff369bad4d9c1e07
martak12 <martakefale488@gmail.com>
2026-03-04 03:39:38 -0800

    new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-//import "./App.css";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div>WELLCOME TO MY FIRST APP</div>
+      <div>
+        WELLCOME TO MY FIRST APP
+        <p>this is marta</p>
+      </div>
     </>
   );
 }
---
+++ src/Funofme.jsx
@@ -0,0 +1,9 @@
+function Funofme() {
+  return (
+    <div>
+      <p>this is fun fucts about</p>
+    </div>
+  );
+}
+
+export default Funofme;
